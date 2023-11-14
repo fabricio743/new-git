d54e5b0bffefd1e3fd8af8fdb20d8187657b92b2
hey-afro <125013840+hey-afro@users.noreply.github.com>
2023-11-14 06:02:12 -0300

    sites.py

---
+++ .py/guardando-info.py
@@ -0,0 +1,6 @@
+nome = input('Qual e seu nome? ')
+cidade = input('Em qual cidade você nasceu? ')
+idade = input('Qual e sua idade? ')
+peso = input('Qual e seu peso? ')
+
+print(f'Olá {nome}, você nasceu na cidade de {cidade} e possui {idade} anos de idade, pesando {peso}kg.')
---
+++ .py/projeto-calculocomdoisnumeros.py
@@ -0,0 +1,5 @@
+a = float(input('Digite um numero: '))
+b = float(input('Digite outro numero: '))
+c = int(a+b)
+
+print(f'A soma de seus dois numeros e : {c}')
